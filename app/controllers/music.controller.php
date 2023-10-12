456cdefb118f2945a178a90bc5a5666bcad8662e
koides <braianricci@gmail.com>
2023-10-12 16:25:35 -0300

    a

--- app/controllers/music.controller.php
+++ app/controllers/music.controller.php
@@ -2,23 +2,43 @@
 require_once './app/views/music.view.php';
 require_once './app/models/music.model.php';
 
-class MusicController
-{
+class MusicController {
     private $model;
     private $view;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->model = new MusicModel();
         $this->view = new MusicView();
     }
 
-    public function listMusic()
-    {
+    public function listMusic() {
         //le pedimos la lista de musica al modelo
         $music = $this->model->getMusic();
 
         //mandamos la lista a la vista para que la muestre
         $this->view->showMusic($music);
     }
+
+    public function addMusic() {
+        //consigo los datos del usuario
+        $cancion = $_POST['cancion'];
+        $album = $_POST['album'];
+        $duracion = $_POST['duracion'];
+        $track = $_POST['track'];
+
+        //validaciones
+        if (empty($cancion) || empty($album) || empty($duracion) || empty($track)) {
+            //$this->view->showError("Debe completar todos los campos");
+            return;
+        }
+
+        $id = $this->model->insertMusic($cancion, $album, $duracion, $track);
+        if ($id) {
+            header('Location: ' . BASE_URL);
+        } else {
+            header('Location: ' . BASE_URL);
+            echo "cuak";
+            //$this->view->showError("Error al insertar la tarea");
+        }
+    } 
 }
--- app/models/music.model.php
+++ app/models/music.model.php
@@ -3,18 +3,23 @@ class MusicModel
 {
     private $db;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->db = new PDO('mysql:host=localhost; dbname=musica; charset=utf8', 'root', '');
     }
 
-    public function getMusic()
-    {
+    public function getMusic() {
         $query = $this->db->prepare('SELECT * FROM canciones');
         $query->execute();
 
         $music = $query->fetchAll(PDO::FETCH_OBJ);
 
         return $music;
+    }
+    
+    public function insertMusic($cancion, $album, $duracion, $track) {
+        $query = $this->db->prepare('INSERT INTO canciones(cancion_nombre, album, duracion, track) VALUES(?,?,?,?)');
+        $query->execute([$cancion, $album, $duracion, $track]);
+
+        return $this->db->lastInsertId();
     }
 }
